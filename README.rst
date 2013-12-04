d1283b4511ef5ce68012c2c206d143d35743338c
philippjfr <P.Rudiger@ed.ac.uk>
2013-12-04 17:39:11 +0000

    Added initial files to the repository

---
+++ README.rst
@@ -0,0 +1,11 @@
+FeatureMapper
+======
+
+FeatureMapper coordinates the presentation of input patterns, collating and analysing the responses.
+
+
+Installation
+============
+
+FeatureMapper requires `NumPy <http://numpy.scipy.org/>`_, `PIL <http://www.pythonware.com/products/pil/>`_, `ImaGen's website <http://ioam.github.com/imagen/>`_  and `Param <http://ioam.github.com/param/>`_.
+
---
+++ setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import sys
+from distutils.core import setup
+
+
+setup_args = {}
+
+#############################################################################################
+##### CEBALERT: copied from topographica; should be simplified
+
+required = {'param':">=0.0.1",
+            'imagen':">=1.0",
+            'numpy':">=1.0"} 
+
+# optional = {}
+            
+packages_to_install = [required]
+packages_to_state = [required]
+
+setup_args = {}
+
+if 'setuptools' in sys.modules:
+    # support easy_install without depending on setuptools
+    install_requires = []
+    for package_list in packages_to_install:
+        install_requires+=["%s%s"%(package,version) for package,version in package_list.items()]
+    setup_args['install_requires']=install_requires
+    setup_args['dependency_links']=["http://pypi.python.org/simple/"]
+    setup_args['zip_safe']=False # CEBALERT: probably ok for imagen; haven't checked
+
+for package_list in packages_to_state:
+    requires = []
+    requires+=["%s (%s)"%(package,version) for package,version in package_list.items()]
+    setup_args['requires']=requires
+
+#############################################################################################
+
+
+setup_args.update(dict(
+    name='FeatureMapper',
+    version='1.0',
+    description='FeatureMapper coordinates the presentation of input patterns, collating and analysing the responses.',
+    long_description=open('README.rst').read(),
+    author= "IOAM",
+    author_email= "developers@topographica.org",
+    maintainer= "IOAM",
+    maintainer_email= "developers@topographica.org",
+    platforms=['Windows', 'Mac OS X', 'Linux'],
+    license='BSD',
+    url='http://ioam.github.com/featuremapper/',
+    packages = ["featuremapper"],
+    classifiers = [
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries"]
+))
+
+
+if __name__=="__main__":
+    setup(**setup_args)
