bd8ed6f162d77081e415205ada8fc9577be7e784
philippjfr <P.Rudiger@ed.ac.uk>
2014-04-01 21:51:14 +0100

    decode_feature now implemented as a ViewOperation

--- featuremapper/analysis/__init__.py
+++ featuremapper/analysis/__init__.py
@@ -1,56 +1,88 @@
+import numpy as np
+
 import param
 
-from featuremapper.distribution import Distribution, DSF_WeightedAverage, DSF_MaxValue
+from imagen.analysis import ViewOperation
+from dataviews import TableView, SheetView
 
-def decode_feature(sheet, preference_map = "OrientationPreference", axis_bounds=(0.0,1.0), cyclic=True, weighted_average=True, cropfn=lambda(x):x):
+from featuremapper.distribution import Distribution, DSF_WeightedAverage, \
+    DSF_MaxValue
+
+
+class decode_feature(ViewOperation):
     """
-    Estimate the value of a feature from the current activity pattern on a sheet.
-
-    The specified preference_map should be measured before this
-    function is called.
+    Estimate the value of a feature from the current activity pattern
+    on a sheet and a preference map of the sheet. The activity and
+    preference should be supplied as an Overlay or a Stack of Overlay
+    objects.
 
     If weighted_average is False, the feature value returned is the
     value of the preference_map at the maximally active location.
 
     If weighted_average is True, the feature value is estimated by
     weighting the preference_map by the current activity level, and
-    averaging the result across all units in the sheet.  The
-    axis_bounds specify the allowable range of the feature values in
-    the preference_map.  If cyclic is true, a vector average is used;
-    otherwise an arithmetic weighted average is used.
+    averaging the result across all units in the sheet.
 
     For instance, if preference_map is OrientationPreference (a cyclic
     quantity), then the result will be the vector average of the
     activated orientations.  For an orientation map this value should
     be an estimate of the orientation present on the input.
 
-    If desired, a cropfn can be supplied that will narrow the analysis
-    down to a specific region of the array; this function will be
-    applied to the preference_map and to the activity array before
-    decoding.  Examples:
+    Record the V1 activity in response to an oriented Gaussian and the
+    orientation preference as follows and overlay them::
 
-    Decode whole area:
+       act = measure_response(pattern_generator=imagen.Gaussian(),
+                              durations=[0.1, 0.5, 1.0])['V1']
+       pref = measure_or_pref()['V1'].top
+       ActivityPreferenceStack = act * pref
 
-       decode_feature(topo.sim["V1"])
-
-    Decode left half only:
-
-       r,c = topo.sim["V1"].activity.shape
-       lefthalf  = lambda(x): x[:,0:c/2]
-       righthalf = lambda(x): x[:,c/2:]
-
-       decode_feature(topo.sim["V1"], cropfn=lefthalf)
-
+       decode_feature(ActivityPreferenceStack)
     """
 
-    d = Distribution(axis_bounds, cyclic)
+    reference_value = param.Number(default=None, allow_None=True, doc="""
+        Allows specifying a reference value, to compute the error of the
+        decoded value.""")
 
-    if not (preference_map in sheet.views.maps):
-        param.Parameterized.warning(preference_map + " should be measured before calling decode_feature.")
-    else:
-        v = sheet.views.maps[preference_map]
-        for (p,a) in zip(cropfn(v.view()[0]).ravel(),
-                         cropfn(sheet.activity).ravel()): d.add({p:a})
+    weighted_average = param.Boolean(default=True, doc="""
+        Decode as vector average if True or by maximal responding unit.""")
 
-    res = DSF_WeightedAverage()(d) if weighted_average else DSF_MaxValue()(d)
-    return res['']['preference']
+    def _process(self, overlay):
+        preference = None; selectivity = None; activity = None;
+        for sv in overlay:
+            if sv.label.endswith('Preference'):
+                preference = sv
+                fname = sv.label[:-len('Preference')]
+            if sv.label.endswith('Selectivity'):
+                selectivity = sv
+            if sv.label.endswith('Activity'):
+                activity = sv
+
+        if preference is None or activity is None:
+            raise ValueError("DecodeFeature requires overlay with activity "
+                             "and preference as input.")
+        if selectivity is None:
+            selectivity = SheetView(np.ones(preference.data.shape),
+                                    preference.bounds)
+
+        cr = preference.cyclic_range
+        cyclic = False if cr is None else True
+        range = (0, 1.0 if cr is None else cr)
+
+        d = Distribution(range, cyclic)
+        for (p, a, s) in zip(preference.data.ravel(), activity.data.ravel(), selectivity.data.ravel()):
+            d.add({p: a*s})
+
+        decoded_label = "Decoded " + fname
+
+        res = DSF_WeightedAverage()(d) if self.p.weighted_average else DSF_MaxValue()(d)
+        decoded_value = res['']['preference']
+        ret = {decoded_label: decoded_value}
+
+        if self.p.feature_estimate is not None:
+            difference = abs(decoded_value - self.p.reference_value)
+            difference = cr - difference if difference >= 0.5*cr else difference
+            if cyclic: difference = difference % cr
+            ret.update({"Decoding Error": difference})
+
+        return [TableView(ret)]
+
